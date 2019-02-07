991b97e2d4f9a7e7240f26c852ac29ff062609cf
Alan Lima <alanlimamarques6@gmail.com>
2019-02-07 09:46:22 -0200

    Importanto um objeto com múltiplos materiais do blender
    Bake materials : https://www.youtube.com/watch?v=Jvj03I81Pl4
    
    Para importar : Aplicar modificadores (na ordem certa), retirar partículas,  juntar objetos e depois do bake deixar só um mateiral, no unity jogar a textura resultante no material do objeto
    
    Mesh collider muito pesado, diminuir vertices ou criar outra mesh low poly para usar no colisor do objeto

---
+++ Unity/teste/Assets/gira.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gira : MonoBehaviour {
+
+	// Use this for initialization
+	public int speed=10;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward*Time.deltaTime*speed);
+		
+	}
+}
